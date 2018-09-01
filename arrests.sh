daf4f5b9f3db36c6bdc14aa9d7abc0ee0433757e
Patrick Conwell <p.conwell@vanderbilt.edu>
2018-09-01 09:29:45 -0500

    minor updates to get script working

--- arrests.sh
+++ arrests.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+python3 arrests.py
+
 ## Set your github username and token
-user=<your github user name>
-token=<your github Personal Access Token>
-repo=<your repo name>
-file=<your file name with file ext>
+user=pconwell
+token=<token here>
+repo=dcso_arrests
+file=arrests.csv
+
+echo $user
 
 ## Get the current file from github so we can check for changes
 github_file='"'`curl --silent --no-buffer --request GET --user "$user:$token" https://api.github.com/repos/$user/$repo/contents/$file | jq -r '.content' | tr -d '\n'`'"'
@@ -13,7 +17,7 @@ github_file='"'`curl --silent --no-buffer --request GET --user "$user:$token" ht
 github_hash='"'`curl  https://api.github.com/repos/$user/$repo/contents/$file | grep sha | cut -d '"' -f4`'"'
 
 ## Create a base64 version of the current config file (to check against $github_file and also upload if there are changes)
-local_file='"'`cat ./$file | base64 | tr -d '\n'`'"'
+local_file='"'`cat /home/ec2-user/arrests/$file | base64 | tr -d '\n'`'"'
 
 ## Check if local version is same as github version
 if [ "$github_file" == "$local_file" ];
@@ -21,11 +25,11 @@ if [ "$github_file" == "$local_file" ];
 ## If the versions are the same, print "true" (this isn't necessarily important, but can be output to a log, for example)
 then
 
-	echo true;
-	
+        echo true;
+
 ## If the versions are different, upload the new version to github
 else
 
-	curl --request PUT --user "$user:$token" --data '{"message": "automated backup", "content": '"$local_file"', "sha": '"$github_hash"'}' https://api.github.com/repos/$user/$repo/contents/$file
+        curl --request PUT --user "$user:$token" --data '{"message": "updated arrests records", "content": '"$local_file"', "sha": '"$github_hash"'}' https://api.github.com/repos/$user/$repo/contents/$file
 
 fi
