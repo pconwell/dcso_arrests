453223b4ac6ed7d7c51ff16dc1496ad1faca2e92
Patrick Conwell <patrick.conwell@gmail.com>
2018-12-10 08:04:02 -0600

    reverted language for git commit message

--- arrests.sh
+++ arrests.sh
@@ -30,7 +30,7 @@ then
 ## If the versions are different, upload the new version to github
 else
 
-        DATA='{"message": "update", "content": '"$local_file"', "sha": '"$github_hash"'}'
+        DATA='{"message": "updated arrests records", "content": '"$local_file"', "sha": '"$github_hash"'}'
         echo $DATA  | curl -X PUT -u "$user:$token" -d @- https://api.github.com/repos/$user/$repo/contents/$file
 
 fi
