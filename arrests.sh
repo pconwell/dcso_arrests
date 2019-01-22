a52370d6152bf33bbcaea2b4eb15da9f34ee7c08
Patrick Conwell <patrick.conwell@gmail.com>
2019-01-22 16:33:26 -0600

    trying to prevent infinite loop in ci

--- arrests.sh
+++ arrests.sh
@@ -11,6 +11,7 @@ repo=dcso_arrests
 file=arrests.csv
 
 echo $user
+echo $TRAVIS_EVENT_TYPE
 
 ## Get the current file from github so we can check for changes
 github_file='"'`curl --silent --no-buffer --request GET --user "$user:$token" https://api.github.com/repos/$user/$repo/contents/$file | jq -r '.content' | tr -d '\n'`'"'
@@ -27,13 +28,23 @@ if [ "$github_file" == "$local_file" ];
 ## If the versions are the same, print "true" (this isn't necessarily important, but can be output to a log, for example)
 then
 
-	echo true;
+	echo "version are the same -- passing";
 
 ## If the versions are different, upload the new version to github
 else
 
-        DATA='{"message": "update", "content": '"$local_file"', "sha": '"$github_hash"'}'
-        echo $DATA  | curl -X PUT -u "$user:$token" -d @- https://api.github.com/repos/$user/$repo/contents/$file
+        if [ "$TRAVIS_EVENT_TYPE" == "cron"];
+	
+	then
+	
+                DATA='{"message": "update", "content": '"$local_file"', "sha": '"$github_hash"'}'
+                echo $DATA  | curl -X PUT -u "$user:$token" -d @- https://api.github.com/repos/$user/$repo/contents/$file
+	
+	else
+	        
+		echo "skipping to prevent infinite loop in CI";
+		
+        fi
 
 fi
 
